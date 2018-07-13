b6018d03b3e17cca97046f91928dcc464f038645
Mario Zugaj <mario@mariozugaj.com>
2018-07-13 13:46:29 +0200

    Add forecasts redux module

---
+++ src/redux/modules/forecasts.js
@@ -0,0 +1,81 @@
+import { coordinatesToString } from "../../utils/utils";
+import * as API from "../../api";
+
+const FETCH_FORECAST_BEGIN = "FETCH_FORECAST_BEGIN";
+const FETCH_FORECAST_SUCCESS = "FETCH_FORECAST_SUCCESS";
+const FETCH_FORECAST_FAILURE = "FETCH_FORECAST_FAILURE";
+
+export function fetchForecastSuccess(forecast) {
+  const { latitude, longitude, daily, hourly, timezone } = forecast;
+  return {
+    type: FETCH_FORECAST_SUCCESS,
+    payload: {
+      location: coordinatesToString({ lat: latitude, lng: longitude }),
+      forecast: {
+        daily,
+        hourly,
+        timezone,
+      },
+    },
+  };
+}
+
+export function fetchForecastFailure(error) {
+  return {
+    type: FETCH_FORECAST_FAILURE,
+    error,
+  };
+}
+
+function fetchForecast(location) {
+  return dispatch => {
+    dispatch({ type: FETCH_FORECAST_BEGIN });
+    API.getForecast(location)
+      .then(response => dispatch(fetchForecastSuccess(response)))
+      .catch(error => dispatch(fetchForecastFailure(error)));
+  };
+}
+
+function shouldFetchForecast(state, location) {
+  return false;
+}
+
+export function fetchForecastIfNeeded(location) {
+  return (dispatch, getState) => {
+    if (shouldFetchForecast(getState(), location)) {
+      return dispatch(fetchForecast(location));
+    }
+  };
+}
+
+const initialState = {
+  isFething: false,
+  error: null,
+  forecastsByLocation: {},
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_FORECAST_BEGIN:
+      return {
+        ...state,
+        isFething: true,
+      };
+    case FETCH_FORECAST_SUCCESS:
+      return {
+        ...state,
+        forecastsByLocation: {
+          [action.payload.location]: { ...action.payload.forecast },
+        },
+        isFething: false,
+      };
+    case FETCH_FORECAST_FAILURE:
+      return {
+        ...state,
+        ...action.error,
+        isFething: false,
+      };
+    default:
+      return state;
+  }
+}
--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import currentLocation from "./currentLocation";
+import forecasts from "./forecasts";
 
 export default combineReducers({
   currentLocation,
+  forecasts,
 });
