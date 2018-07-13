ad99829bc5be49f4726d9ded0705ea32063ba2d7
Mario Zugaj <mario@mariozugaj.com>
2018-07-13 11:33:40 +0200

    Add round, geocode and coordinatesToString utils

---
+++ src/utils/utils.js
@@ -0,0 +1,46 @@
+export function round(number, precision = 4) {
+  const shift = (number, precision, reverseShift) => {
+    if (reverseShift) {
+      precision = -precision;
+    }
+    const numArray = ("" + number).split("e");
+    return +(numArray[0] + "e" + (numArray[1] ? +numArray[1] + precision : precision));
+  };
+  return shift(Math.round(shift(number, precision, false)), precision, true);
+}
+
+export function geocode(coordinates) {
+  return new Promise((resolve, reject) => {
+    const geocoder = new window.google.maps.Geocoder();
+
+    geocoder.geocode({ location: coordinates }, (results, status) => {
+      if (status === "OK") {
+        if (results[0]) {
+          const filtered = results.filter(loc => loc.types.includes("locality"));
+          if (filtered[0]) {
+            const {
+              formatted_address,
+              geometry: { location },
+              place_id,
+            } = filtered[0];
+
+            return resolve({
+              label: formatted_address,
+              lat: location.lat(),
+              lng: location.lng(),
+              placeId: place_id,
+            });
+          }
+        } else {
+          return reject(window.alert("No results found"));
+        }
+      } else {
+        return reject(window.alert("Geocoder failed due to: " + status));
+      }
+    });
+  });
+}
+
+export function coordinatesToString({ lat, lng }) {
+  return `${round(lat)},${round(lng)}`;
+}
