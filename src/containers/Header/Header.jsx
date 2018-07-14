af837c38ccd0a349ec91112382545676a5894718
Mario Zugaj <mario@mariozugaj.com>
2018-07-14 17:56:23 +0200

    Extract Logo into separate component + refactor Header

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,37 +1,24 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
-import Icon from "Icon";
+import Logo from "Logo";
 import LocationInfo from "LocationInfo";
 import Search from "Search";
 import CurrentPosition from "CurrentPosition";
 import { setLocation, getCurrentPosition, startSearching, stopSearching } from "currentLocation";
 
 export class Header extends Component {
-  componentDidMount() {}
-
   render() {
     return (
-      <header className="layout-container">
-        <section className="page-header">
-          <Link to="/" className="page-header__icon page-header__icon--onTop">
-            <Icon
-              name="logo"
-              className="page-header__icon--img page-header__logo"
-              title="Go to homepage"
-              alt="BlueSun Weather Forecast"
-            />
-          </Link>
-          <LocationInfo
-            location={this.props.currentLocation}
-            startSearching={this.props.startSearching}
-          />
+      <section className="layout-container">
+        <header className="page-header">
+          <Logo />
+          <LocationInfo {...this.props} />
           <CurrentPosition {...this.props} />
           <Search {...this.props} />
-        </section>
-      </header>
+        </header>
+      </section>
     );
   }
 }
