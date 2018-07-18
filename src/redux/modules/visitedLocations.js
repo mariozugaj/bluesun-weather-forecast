ff62b0501c0d811d3475d6b234fc21ec7ee843bd
chanjman <mariozugaj@gmail.com>
2018-07-18 17:09:10 +0200

    Change visiting location once => always visiting

--- src/redux/modules/visitedLocations.js
+++ src/redux/modules/visitedLocations.js
@@ -2,41 +2,38 @@ import { coordinatesToString } from "helpers";
 
 const VISIT_LOCATION = "VISIT_LOCATION";
 
-function visitLocation(location) {
+export function visitLocation(location) {
   const coordinates = coordinatesToString({ lat: location.lat, lng: location.lng });
   return {
     type: VISIT_LOCATION,
     payload: {
       coordinates,
       location: { ...location },
+      visitedAt: Date.now(),
     },
   };
 }
 
-function shouldVisitLocation(state, { lat, lng }) {
-  const coordinates = coordinatesToString({ lat, lng });
-  const location = state.visitedLocations[coordinates];
-
-  if (!location) return true;
-  return false;
-}
-
-export function visitLocationIfNeeded(location) {
-  return (dispatch, getState) => {
-    if (shouldVisitLocation(getState(), location)) {
-      return dispatch(visitLocation(location));
-    }
-  };
-}
-
-const initialState = {};
+const initialState = {
+  "45.815,15.9819": {
+    id: "45.815,15.9819",
+    visitedAt: 1531919115731,
+    label: "Zagreb, Croatia",
+    lat: 45.815,
+    lng: 15.9819,
+  },
+};
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case VISIT_LOCATION:
       return {
         ...state,
-        [action.payload.coordinates]: { ...action.payload.location },
+        [action.payload.coordinates]: {
+          id: `${action.payload.coordinates}`,
+          visitedAt: action.payload.visitedAt,
+          ...action.payload.location,
+        },
       };
     default:
       return state;
