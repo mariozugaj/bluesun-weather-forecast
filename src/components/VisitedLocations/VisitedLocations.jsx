acb3e3893f3d1a09cb87e2df0489c3679d707141
Mario Zugaj <mario@mariozugaj.com>
2018-07-18 17:13:44 +0200

    Add HomePage and VisitedLocations components

---
+++ src/components/VisitedLocations/VisitedLocations.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Table, HeaderRow, Row } from "components/Table";
+import Icon from "components/Icon";
+import { nextDays, round } from "helpers";
+
+const WeatherSymbol = ({ summary = "", condition = "", temperature = 0 }) => {
+  return (
+    <figure className="weather-symbol__wrapper">
+      <Icon name={condition} title={summary} size={50} />
+      <span className="temperature--warm">{round(temperature, 0)}°</span>
+    </figure>
+  );
+};
+
+WeatherSymbol.propTypes = {
+  summary: PropTypes.string.isRequired,
+  condition: PropTypes.string.isRequired,
+  temperature: PropTypes.number.isRequired,
+};
+
+const VisitedLocations = ({ visitedLocations = [], forecast = {} }) => {
+  const renderRows = () => {
+    return visitedLocations.map(location => {
+      const forecastForLocation = forecast.byLocation[location.id];
+      const rowContent =
+        forecastForLocation &&
+        forecastForLocation.daily.data
+          .slice(0, 5)
+          .map(day => (
+            <WeatherSymbol
+              condition={day.icon}
+              summary={day.summary}
+              temperature={day.apparentTemperatureHigh}
+              key={day.time}
+            />
+          ));
+
+      return (
+        <Row to={`/forecast/daily/${location.id}`} key={location.id} size={5}>
+          <h3>{location.label}</h3>
+          {rowContent}
+        </Row>
+      );
+    });
+  };
+  const headerRowContent = nextDays(5)();
+
+  return (
+    <React.Fragment>
+      <h2 className="visited-locations__title">Recent locations</h2>
+      <Table size={5}>
+        <HeaderRow content={headerRowContent} size={5} />
+        {renderRows()}
+      </Table>
+    </React.Fragment>
+  );
+};
+
+VisitedLocations.propTypes = {
+  visitedLocations: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      visitedAt: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired,
+      lat: PropTypes.number,
+      lng: PropTypes.number,
+    })
+  ).isRequired,
+  forecast: PropTypes.shape({
+    isFething: PropTypes.bool.isRequired,
+    byLocation: PropTypes.object.isRequired,
+  }).isRequired,
+};
+
+export default VisitedLocations;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,33 +1,68 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import WeatherLocationList from "WeatherLocationList";
 import { clearLocation } from "modules/currentLocation";
 import { fetchForecastIfNeeded } from "modules/forecast";
+import VisitedLocations from "components/VisitedLocations";
+
+const EmptyHomeStatus = () => (
+  <div className="center-page-text-wrapper">
+    <h2>As it turns out, you haven't yet visited any locations.</h2>
+  </div>
+);
 
 export class HomePage extends Component {
   componentDidMount() {
-    this.props.clearLocation();
-    Object.keys(this.props.visitedLocations).forEach(location => {
-      return this.props.fetchForecastIfNeeded(location);
+    const { clearLocation, visitedLocations, fetchForecastIfNeeded } = this.props;
+
+    clearLocation();
+    visitedLocations.forEach(location => {
+      return fetchForecastIfNeeded(location.id);
     });
   }
 
   render() {
-    const { forecasts, visitedLocations } = this.props;
+    const { forecast, visitedLocations } = this.props;
+    const anyVisited = visitedLocations.length !== 0;
 
     return (
       <main className="layout-container">
-        <WeatherLocationList forecasts={forecasts} visitedLocations={visitedLocations} />
+        {anyVisited ? (
+          <VisitedLocations forecast={forecast} visitedLocations={visitedLocations} />
+        ) : (
+          <EmptyHomeStatus />
+        )}
       </main>
     );
   }
 }
 
+HomePage.propTypes = {
+  visitedLocations: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      visitedAt: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired,
+      lat: PropTypes.number,
+      lng: PropTypes.number,
+    })
+  ).isRequired,
+  forecast: PropTypes.shape({
+    isFething: PropTypes.bool.isRequired,
+    byLocation: PropTypes.object.isRequired,
+  }).isRequired,
+};
+
+const mostRecentFiveLocations = allLocations =>
+  Object.values(allLocations)
+    .slice(0, 5)
+    .sort((a, b) => (a.visitedAt < b.visitedAt ? 1 : -1));
+
 const mapState = state => {
   return {
-    visitedLocations: state.visitedLocations,
-    forecasts: state.forecasts,
+    visitedLocations: mostRecentFiveLocations(state.visitedLocations),
+    forecast: state.forecast,
   };
 };
 
