7d6cd8e328e5945a5016fbc9ede6d31289603799
chanjman <mariozugaj@gmail.com>
2018-07-19 07:59:34 +0200

    Add ForecastDailyPage component

--- src/pages/ForecastDailyPage/ForecastDailyPage.jsx
+++ src/pages/ForecastDailyPage/ForecastDailyPage.jsx
@@ -1,11 +1,60 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+
+import { setLocationFromParams, clearLocation } from "modules/currentLocation";
+import { fetchForecastIfNeeded } from "modules/forecast";
+import { extractCoordinates } from "helpers";
+import CurrentConditions from "components/CurrentConditions";
 
 export class ForecastDailyPage extends Component {
-  state = {};
+  componentDidMount() {
+    this.setLocationAndForecast();
+  }
+
+  componentDidUpdate(prevProps) {
+    this.setLocationAndForecast();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      nextProps.isFetching !== this.props.isFetching ||
+      nextProps.location.pathname !== this.props.location.pathname
+    );
+  }
+
+  setLocationAndForecast = () => {
+    this.props.setLocationFromParams();
+    this.props.fetchForecastIfNeeded();
+  };
 
   render() {
-    return <div>Daily</div>;
+    return <CurrentConditions forecast={this.props.forecast} isFetching={this.props.isFetching} />;
   }
 }
 
-export default ForecastDailyPage;
+const mapState = (state, ownProps) => {
+  return {
+    currentLocation: state.currentLocation,
+    forecast: state.forecast.byLocation[ownProps.match.params.coordinates],
+    isFetching: state.forecast.isFetching,
+  };
+};
+
+const mapDispatch = (dispatch, ownProps) => {
+  const coordinates = extractCoordinates(ownProps.match.params.coordinates);
+  return {
+    setLocationFromParams: () => {
+      return dispatch(setLocationFromParams(coordinates));
+    },
+    clearLocation: () => dispatch(clearLocation()),
+    fetchForecastIfNeeded: () => dispatch(fetchForecastIfNeeded(ownProps.match.params.coordinates)),
+  };
+};
+
+export default withRouter(
+  connect(
+    mapState,
+    mapDispatch
+  )(ForecastDailyPage)
+);
