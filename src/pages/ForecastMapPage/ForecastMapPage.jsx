485fa6d5cbae304295ece54adfc2745189e4f9fe
chanjman <mariozugaj@gmail.com>
2018-07-19 18:11:58 +0200

    Add ForecastMapPage component

--- src/pages/ForecastMapPage/ForecastMapPage.jsx
+++ src/pages/ForecastMapPage/ForecastMapPage.jsx
@@ -1,11 +1,50 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import DarkskyMap from "react-darksky-map";
+
+import { setLocationFromParams } from "modules/currentLocation";
+import { extractCoordinates } from "helpers";
 
 export class ForecastMapPage extends Component {
-  state = {};
+  componentDidMount() {
+    this.props.setLocationFromParams();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.location.pathname !== this.props.location.pathname) {
+      this.props.setLocationFromParams();
+    }
+  }
 
   render() {
-    return <div>Map</div>;
+    const { lat, lng } = this.props.currentLocation;
+    return (
+      <section className="map-wrapper">
+        <DarkskyMap lat={lat} lng={lng} zoom={7} height="100%" />
+      </section>
+    );
   }
 }
 
-export default ForecastMapPage;
+const mapState = (state, ownProps) => {
+  return {
+    currentLocation: state.currentLocation,
+  };
+};
+
+const mapDispatch = (dispatch, ownProps) => {
+  const coordinates = extractCoordinates(ownProps.match.params.coordinates);
+  return {
+    setLocationFromParams: () => {
+      return dispatch(setLocationFromParams(coordinates));
+    },
+  };
+};
+
+export default withRouter(
+  connect(
+    mapState,
+    mapDispatch
+  )(ForecastMapPage)
+);
