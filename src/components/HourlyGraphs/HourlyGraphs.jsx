a466e1c30db5a72a9ebb114fc2e8660401416d27
Mario Zugaj <mario@mariozugaj.com>
2018-07-20 23:34:00 +0200

    Refactor HourlyGraphs component

--- src/components/HourlyGraphs/HourlyGraphs.jsx
+++ src/components/HourlyGraphs/HourlyGraphs.jsx
@@ -3,43 +3,66 @@ import { extent } from "d3-array";
 
 import TemperatureGraph from "./TemperatureGraph";
 import PrecipitationGraph from "./PrecipitationGraph";
+import WindGraph from "./WindGraph";
+import UVIndexGraph from "./UVIndexGraph";
 
 const HourlyGraphs = ({ forecast }) => {
-  const temperatureData = forecast.data.map(data => {
-    const { temperature, time } = data;
-    return { temperature, time };
-  });
+  const dataFor = key => {
+    return forecast.data.map(data => {
+      return { [key]: data[key], time: data.time };
+    });
+  };
+  const minMax = (data, callback) => extent(data, callback);
+  const largeHeight = diff => (diff[1] - diff[0] > 15 ? 300 : 250);
+  const mediumHeight = diff => (diff[1] - diff[0] > 5 ? 250 : 150);
+  const smallHeight = diff => (diff[1] - diff[0] > 5 ? 250 : 100);
 
-  const precipitationData = forecast.data.map(data => {
-    const { precipIntensity, time } = data;
-    return { precipIntensity, time };
-  });
+  const anyPrecipitation = dataFor("precipIntensity").some(
+    dataPoint => dataPoint.precipIntensity > 0.05
+  );
 
-  const diff = (data, callback) => extent(data, callback);
-
-  const tempDiff = diff(temperatureData, d => d.temperature);
-  const tempHeight = tempDiff[1] - tempDiff[0] > 15 ? 300 : 250;
-
-  const anyPrecipitation = precipitationData.some(dataPoint => dataPoint.precipIntensity > 0);
-  const precipDiff = diff(precipitationData, d => d.precipIntensity);
-  const precipHeight = precipDiff[1] - precipDiff[0] > 5 ? 250 : 100;
+  const height = (key, size) => {
+    const diff = minMax(dataFor(key), d => d[key]);
+    switch (size) {
+      case "small":
+        return smallHeight(diff);
+      case "medium":
+        return mediumHeight(diff);
+      case "large":
+        return largeHeight(diff);
+      default:
+        return largeHeight(diff);
+    }
+  };
 
   return (
     <React.Fragment>
       <TemperatureGraph
         width={1235}
-        height={tempHeight}
-        margin={{ left: 30, top: 40, right: 20, bottom: 30 }}
-        data={temperatureData}
+        height={height("temperature", "large")}
+        margin={{ left: 35, top: 40, right: 20, bottom: 30 }}
+        data={dataFor("temperature")}
       />
       {anyPrecipitation && (
         <PrecipitationGraph
           width={1235}
-          height={precipHeight}
+          height={height("precipIntensity", "small")}
           margin={{ left: 30, top: 40, right: 20, bottom: 20 }}
-          data={precipitationData}
+          data={dataFor("precipIntensity")}
         />
       )}
+      <WindGraph
+        width={1235}
+        height={height("windSpeed", "medium")}
+        margin={{ left: 30, top: 40, right: 20, bottom: 20 }}
+        data={dataFor("windSpeed")}
+      />
+      <UVIndexGraph
+        width={1235}
+        height={height("uvIndex", "medium")}
+        margin={{ left: 30, top: 40, right: 20, bottom: 20 }}
+        data={dataFor("uvIndex")}
+      />
     </React.Fragment>
   );
 };
