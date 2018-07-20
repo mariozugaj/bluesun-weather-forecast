c519e1957ada8b828206182422e79af5ce85a04e
chanjman <mariozugaj@gmail.com>
2018-07-20 15:30:49 +0200

    Add HourlyGraphs component

---
+++ src/components/HourlyGraphs/HourlyGraphs.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { extent } from "d3-array";
+
+import TemperatureGraph from "./TemperatureGraph";
+import PrecipitationGraph from "./PrecipitationGraph";
+
+const HourlyGraphs = ({ forecast }) => {
+  const temperatureData = forecast.data.map(data => {
+    const { temperature, time } = data;
+    return { temperature, time };
+  });
+
+  const precipitationData = forecast.data.map(data => {
+    const { precipIntensity, time } = data;
+    return { precipIntensity, time };
+  });
+
+  const diff = (data, callback) => extent(data, callback);
+
+  const tempDiff = diff(temperatureData, d => d.temperature);
+  const tempHeight = tempDiff[1] - tempDiff[0] > 15 ? 300 : 250;
+
+  const precipDiff = diff(precipitationData, d => d.precipIntensity);
+  const precipHeight = precipDiff[1] - precipDiff[0] > 5 ? 200 : 100;
+
+  return (
+    <React.Fragment>
+      <TemperatureGraph
+        width={1235}
+        height={tempHeight}
+        margin={{ left: 30, top: 40, right: 20, bottom: 30 }}
+        data={temperatureData}
+      />
+      <PrecipitationGraph
+        width={1235}
+        height={precipHeight}
+        margin={{ left: 30, top: 40, right: 20, bottom: 30 }}
+        data={precipitationData}
+      />
+    </React.Fragment>
+  );
+};
+
+export default HourlyGraphs;
