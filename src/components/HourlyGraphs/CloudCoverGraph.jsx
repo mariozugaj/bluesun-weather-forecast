a7148cd31e129a01190d51356122dec84b822757
Mario Zugaj <mariozugaj@gmail.com>
2018-07-21 23:45:03 +0200

    Add tooltip to CloudCoverGraph component

--- src/components/HourlyGraphs/CloudCoverGraph.jsx
+++ src/components/HourlyGraphs/CloudCoverGraph.jsx
@@ -4,10 +4,26 @@ import { Group } from "@vx/group";
 import { curveBasis } from "@vx/curve";
 import { AxisLeft } from "@vx/axis";
 import { scaleTime, scaleLinear } from "@vx/scale";
-import { extent, max } from "d3-array";
-import { LinePath } from "@vx/shape";
+import { extent, max, bisector } from "d3-array";
+import { LinePath, Line, Bar } from "@vx/shape";
+import { Point } from "@vx/point";
+import { withTooltip, Tooltip } from "@vx/tooltip";
+import { localPoint } from "@vx/event";
+import { timeFormat } from "d3-time-format";
 
-export const CloudCoverGraph = ({ width, height, margin, data }) => {
+export const CloudCoverGraph = ({
+  width,
+  height,
+  margin,
+  data,
+  showTooltip,
+  tooltipData,
+  tooltipLeft,
+  tooltipTop,
+  tooltipOpen,
+  hideTooltip,
+  events,
+}) => {
   if (width < 10) return null;
 
   const xMax = width - margin.left - margin.right;
@@ -28,6 +44,26 @@ export const CloudCoverGraph = ({ width, height, margin, data }) => {
   });
 
   const numTicksForHeight = height => (height > 150 ? 8 : 5);
+  const bisectDate = bisector(d => new Date(d.time * 1000)).left;
+  const formatDate = timeFormat("%a %b %d, %H:%M");
+
+  const handleTooltip = datum => event => {
+    let xLoc = localPoint(event).x;
+    xLoc -= margin.left;
+    const x0 = xScale.invert(xLoc);
+    const index = bisectDate(data, x0, 1);
+    const d0 = data[index - 1];
+    const d1 = data[index];
+    let d = d0;
+    if (d1 && d1.time) {
+      d = x0 - x(d0.time) > x(d1.time) - x0 ? d1 : d0;
+    }
+    return showTooltip({
+      tooltipData: d,
+      tooltipLeft: xLoc,
+      tooltipTop: yScale(d.cloudCover * 100),
+    });
+  };
 
   return (
     <figure className="graph-wrapper">
@@ -58,6 +94,41 @@ export const CloudCoverGraph = ({ width, height, margin, data }) => {
             strokeWidth={2}
             curve={curveBasis}
           />
+          <Bar
+            x={0}
+            y={0}
+            width={xMax}
+            height={yMax}
+            fill="transparent"
+            rx={14}
+            data={data}
+            onTouchStart={handleTooltip}
+            onTouchMove={handleTooltip}
+            onMouseMove={handleTooltip}
+            onTouchEnd={() => () => hideTooltip()}
+            onMouseLeave={() => () => hideTooltip()}
+          />
+          {tooltipOpen && (
+            <g>
+              <Line
+                from={new Point({ x: tooltipLeft, y: 0 })}
+                to={new Point({ x: tooltipLeft, y: yMax })}
+                stroke="#0341b6"
+                strokeWidth={2}
+                style={{ pointerEvents: "none" }}
+                strokeDasharray="2,2"
+              />
+              <circle
+                cx={tooltipLeft}
+                cy={tooltipTop}
+                r={6}
+                fill="#0341b6"
+                stroke="white"
+                strokeWidth={2}
+                style={{ pointerEvents: "none" }}
+              />
+            </g>
+          )}
         </Group>
         <AxisLeft
           top={margin.top}
@@ -74,8 +145,24 @@ export const CloudCoverGraph = ({ width, height, margin, data }) => {
         </Group>
         />
       </svg>
+      {tooltipOpen && (
+        <span>
+          <Tooltip
+            top={0}
+            left={tooltipLeft - 12}
+            style={{
+              backgroundColor: "#005bff",
+              color: "white",
+            }}>
+            {`${Math.round(y(tooltipData))}%`}
+          </Tooltip>
+          <Tooltip top={height - margin.bottom} left={tooltipLeft - 25}>
+            {formatDate(x(tooltipData))}
+          </Tooltip>
+        </span>
+      )}
     </figure>
   );
 };
 
-export default CloudCoverGraph;
+export default withTooltip(CloudCoverGraph);
