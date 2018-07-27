d6c93f0629541726aacb2bc8c1ce546b3784e96f
Mario Zugaj <mariozugaj@gmail.com>
2018-07-27 17:23:57 +0200

    Add favoriteLocations module

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -5,6 +5,7 @@ import search from "modules/search";
 import forecast from "modules/forecast";
 import map from "modules/map";
 import currentPosition from "modules/currentPosition";
+import favoriteLocations from "modules/favoriteLocations";
 import recentLocations from "modules/recentLocations";
 
 export default combineReducers({
@@ -13,5 +14,6 @@ export default combineReducers({
   forecast,
   map,
   currentPosition,
+  favoriteLocations,
   recentLocations,
 });
