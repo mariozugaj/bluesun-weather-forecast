5647e81984f53190de3a913456e1d52874a63336
Mario Zugaj <mario@mariozugaj.com>
2018-07-27 12:26:14 +0200

    Add NotFoundPage component/page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
-  return <p>Not Found!</p>;
+  return (
+    <div className="center-page-text-wrapper">
+      <h2>We were unable to find what you were searching for. Try again?</h2>
+      <Link to="/">
+        <button className="redirect-button">Go to the homepage</button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
