9560c178e2bade90fda7b948c759db2c102face5
Mario Zugaj <mario@mariozugaj.com>
2018-07-27 19:23:06 +0200

    Remove redundant imports

--- src/redux/modules/recentLocations.js
+++ src/redux/modules/recentLocations.js
@@ -1,6 +1,4 @@
 import * as actionTypes from "redux/actionTypes";
-import { deleteVisitedLocation } from "modules/locations";
-import { deleteForecastForLocation } from "modules/forecast";
 
 export function addRecentLocation(coordinates) {
   return {
