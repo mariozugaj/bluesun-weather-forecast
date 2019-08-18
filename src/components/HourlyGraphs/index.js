3c7e6c7b89d695358c04de3941e607738b34b057
Mario Zugaj <mario@mariozugaj.com>
2019-08-18 12:37:57 +0200

    Add HumidityGraph to HourlyGraphs

--- src/components/HourlyGraphs/index.js
+++ src/components/HourlyGraphs/index.js
@@ -6,3 +6,4 @@ export { PrecipitationGraph } from "./PrecipitationGraph";
 export { WindGraph } from "./WindGraph";
 export { UVIndexGraph } from "./UVIndexGraph";
 export { CloudCoverGraph } from "./CloudCoverGraph";
+export { HumidityGraph } from "./HumidityGraph";
